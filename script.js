74c9c9ded614f30f23bd9931585efccb302dd583
Insomnium1250 <quang_evilrocker@yahoo.com.vn>
2024-09-29 20:59:41 +0700

    First draft of the website

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-
+let playerChoice = prompt("Enter your choice: rock, paper, or scissors").toLowerCase();
+while playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors" {
+    playerChoice = prompt("Invalid choice. Please enter rock, paper, or scissors.").toLowerCase();
+}
+let playerChoiceNumber;
+if (playerChoice === "rock") {
+    playerChoiceNumber = 1;
+}
+else if (playerChoice === "paper") {
+    playerChoiceNumber = 2;
+}
+else {
+    playerChoiceNumber = 3;
+}
+let computerChoiceNumber = Math.floor(Math.random() * 3) + 1;
+let computerChoice;
+if (computerChoiceNumber === 1) {
+    computerChoice = "rock";
+}
+else if (computerChoiceNumber === 2) {
+    computerChoice = "paper";
+}
+else {
+    computerChoice = "scissors";
+}
+let difference = computerChoiceNumber - playerChoiceNumber;
+if (difference === 0) {
+    alert(`It's a tie! You both chose ${playerChoice}.`);
+}
+else if (difference === -1 || difference === 2) {
+    alert(`You win! Computer chose ${computerChoice}.`);
+}
+else if (difference === 1 || difference === -2) {
+    alert(`You lose! Computer chose ${computerChoice}.`);
+}
