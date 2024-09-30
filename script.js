c43de7948d3dc6d4762a054d46ce777117bd8907
Insomnium1250 <quang_evilrocker@yahoo.com.vn>
2024-09-30 16:20:34 +0700

    Added rounds and game logic Refactor old code into playRound function. The game is now playable, might need to make playGame function in the future.

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
-let playerChoice = prompt("Enter your choice: rock, paper, or scissors").toLowerCase();
-while (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
-    playerChoice = prompt("Invalid choice. Please enter rock, paper, or scissors.").toLowerCase();
+function playRound () {
+    let playerChoice = prompt("Enter your choice: rock, paper, or scissors").toLowerCase();
+    while (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
+        playerChoice = prompt("Invalid choice. Please enter rock, paper, or scissors.").toLowerCase();
+    }
+    let playerChoiceNumber;
+    if (playerChoice === "rock") {
+        playerChoiceNumber = 1;
+    }
+    else if (playerChoice === "paper") {
+        playerChoiceNumber = 2;
+    }
+    else {
+        playerChoiceNumber = 3;
+    }
+    let computerChoiceNumber = Math.floor(Math.random() * 3) + 1;
+    let choices = ["rock", "paper", "scissors"];
+    let computerChoice = choices[computerChoiceNumber - 1];
+    let difference = computerChoiceNumber - playerChoiceNumber;
+    if (difference === 0) {
+        roundNumber--;
+        alert(`It's a tie! You both chose ${playerChoice}. Score is now Player: ${playerScore}, Computer: ${computerScore}.`);
+    }
+    else if (difference === -1 || difference === 2) {
+        playerScore++;
+        alert(`You win! Computer chose ${computerChoice}. Score is now Player: ${playerScore}, Computer: ${computerScore}.`);
+    }
+    else if (difference === 1 || difference === -2) {
+        computerScore++;
+        alert(`You lose! Computer chose ${computerChoice}. Score is now Player: ${playerScore}, Computer: ${computerScore}.`);
+    }
 }
-let playerChoiceNumber;
-if (playerChoice === "rock") {
-    playerChoiceNumber = 1;
-}
-else if (playerChoice === "paper") {
-    playerChoiceNumber = 2;
-}
-else {
-    playerChoiceNumber = 3;
-}
-let computerChoiceNumber = Math.floor(Math.random() * 3) + 1;
-let choices = ["rock", "paper", "scissors"];
-let computerChoice = choices[computerChoiceNumber - 1];
-let difference = computerChoiceNumber - playerChoiceNumber;
-if (difference === 0) {
-    alert(`It's a tie! You both chose ${playerChoice}.`);
-}
-else if (difference === -1 || difference === 2) {
-    alert(`You win! Computer chose ${computerChoice}.`);
-}
-else if (difference === 1 || difference === -2) {
-    alert(`You lose! Computer chose ${computerChoice}.`);
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 0;
+while (roundNumber < 5) {
+    playRound();
+    roundNumber++;
+    if (playerScore === 3) {
+        alert("You win the game!");
+        break;
+    }
+    else if (computerScore === 3) {
+        alert("You lose the game!");
+        break;
+    }
 }
