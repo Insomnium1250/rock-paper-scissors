51d05afe44c85d0f13639c91257f383f6c12d175
Insomnium1250 <quang_evilrocker@yahoo.com.vn>
2024-10-11 16:51:18 +0700

    Refactor round logic to shorter code

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 0;
+
 function playRound () {
     let playerChoice = prompt("Enter your choice: rock, paper, or scissors").toLowerCase();
     while (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
         playerChoice = prompt("Invalid choice. Please enter rock, paper, or scissors.").toLowerCase();
     }
-    let playerChoiceNumber;
-    if (playerChoice === "rock") {
-        playerChoiceNumber = 1;
-    }
-    else if (playerChoice === "paper") {
-        playerChoiceNumber = 2;
-    }
-    else {
-        playerChoiceNumber = 3;
-    }
-    let computerChoiceNumber = Math.floor(Math.random() * 3) + 1;
-    let choices = ["rock", "paper", "scissors"];
-    let computerChoice = choices[computerChoiceNumber - 1];
-    let difference = computerChoiceNumber - playerChoiceNumber;
-    if (difference === 0) {
-        roundNumber--;
-        alert(`It's a tie! You both chose ${playerChoice}. Score is now Player: ${playerScore}, Computer: ${computerScore}.`);
-    }
-    else if (difference === -1 || difference === 2) {
-        playerScore++;
-        alert(`You win! Computer chose ${computerChoice}. Score is now Player: ${playerScore}, Computer: ${computerScore}.`);
-    }
-    else if (difference === 1 || difference === -2) {
-        computerScore++;
-        alert(`You lose! Computer chose ${computerChoice}. Score is now Player: ${playerScore}, Computer: ${computerScore}.`);
-    }
-}
+    const choices = ["rock", "paper", "scissors"];
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    let difference = (choices.length + choices.indexOf(playerChoice) - choices.indexOf(computerChoice)) % choices.length;
 
-let playerScore = 0;
-let computerScore = 0;
-let roundNumber = 0;
+    switch(difference){
+        case 0:
+            roundNumber--;
+            alert(`It's a tie! You both chose ${playerChoice}. Score is now Player: ${playerScore}, Computer: ${computerScore}.`);
+            break;
+        case 2:
+            computerScore++;
+            alert(`You lose! Computer chose ${computerChoice}. Score is now Player: ${playerScore}, Computer: ${computerScore}.`);
+            break;
+        default:
+            playerScore++;
+            alert(`You win! Computer chose ${computerChoice}. Score is now Player: ${playerScore}, Computer: ${computerScore}.`);
+            break;
+    } 
+}
 
 function playGame() {
     playerScore = 0;
@@ -41,9 +33,8 @@ function playGame() {
     roundNumber = 0;
     
     while (roundNumber < 5) {
-        playRound();
         roundNumber++;
-        
+        playRound();
         if (playerScore === 3) {
             alert("You win the game!");
             break;
